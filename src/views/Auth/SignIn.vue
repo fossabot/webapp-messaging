7575d3137bf8c2a692fd52fb7ce76cc45c645ebf
Tomaž Jerman <tomaz.jerman@kendu.si>
2019-03-15 15:07:15 +0100

    Allow login with InAppBrowser

--- src/views/Auth/SignIn.vue
+++ src/views/Auth/SignIn.vue
@@ -1,8 +1,25 @@
 <script>
+/* global cordova */
+
 export default {
   beforeCreate () {
-    const { signInUrl } = window.CrustConfig.webapp.auth || {}
-    window.location = signInUrl || (this.$system.baseURL() + '/oidc')
+    const link = this.$system.baseURL() + '/oidc'
+    if (process.env.VUE_APP_CORDOVA) {
+      const ref = cordova.InAppBrowser.open(link, '_blank', 'hidenavigationbuttons=yes,hideurlbar=yes,zoom=no,location=no,toolbar=no')
+
+      // TODO: Improve this flow
+      ref.addEventListener('loadstop', (event) => {
+        let { url } = event
+        if (url.indexOf('//latest.crust.tech') > -1) {
+          ref.close()
+          this.$router.push({ name: 'root' })
+        }
+      })
+    } else {
+      // For now, we redirect directly to oidc endpoint
+      // that will redirect us to satosa login screen
+      window.location = link
+    }
   },
 
   render () { return null },
