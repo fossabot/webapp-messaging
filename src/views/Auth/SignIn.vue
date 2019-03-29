eeaeb4527099f770fde3b7837aea18196ab12812
Tomaž Jerman <tomaz.jerman@kendu.si>
2019-03-29 09:27:22 +0100

    Improve signin to support multiple domains

--- src/views/Auth/SignIn.vue
+++ src/views/Auth/SignIn.vue
@@ -1,21 +1,35 @@
 <script>
 /* global cordova */
 
+import Vue from 'vue'
+import update from '../../plugins/update'
+
 export default {
   beforeCreate () {
-    const link = this.$system.baseURL() + '/oidc'
     if (process.env.VUE_APP_CORDOVA) {
-      const ref = cordova.InAppBrowser.open(link, '_blank', 'hidenavigationbuttons=yes,hideurlbar=yes,zoom=no,location=no,toolbar=no')
+      window.crustHybrid.configureClient()
+        .then(({ config, domain }) => {
+          // Update config
+          window.CrustConfig = config
 
-      // TODO: Improve this flow
-      ref.addEventListener('loadstop', (event) => {
-        let { url } = event
-        if (url.indexOf('//latest.crust.tech') > -1) {
-          ref.close()
-          this.$router.push({ name: 'root' })
-        }
-      })
+          // Update plugins & state
+          update(Vue, null)
+
+          const link = `${this.$system.baseURL()}/oidc`
+          const ref = cordova.InAppBrowser.open(link, '_blank', 'hidenavigationbuttons=yes,hideurlbar=yes,zoom=no,location=no,toolbar=no')
+
+          // Provide an auth window
+          // TODO: Improve this flow
+          ref.addEventListener('loadstop', (event) => {
+            let { url } = event
+            if (url.indexOf(`//${domain}`) > -1) {
+              ref.close()
+              this.$router.push({ name: 'landing' })
+            }
+          })
+        }, false)
     } else {
+      const link = `${this.$system.baseURL()}/oidc`
       // For now, we redirect directly to oidc endpoint
       // that will redirect us to satosa login screen
       window.location = link
